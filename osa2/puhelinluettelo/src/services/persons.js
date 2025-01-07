0e0d14e2e6a3f52abc249d77bfc5a6f66f490b26
jesse <jesse.tenhula@gmail.com>
2025-01-07 17:44:07 +0200

    vaihdettu backendin url

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://fullstack-open-osa3-k39d.onrender.com/api/persons'
+const baseUrl = 'http://localhost:3001/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
